6f341ab3460c46461563ee3e6395b49bff308d32
Gottlieb613 <charliegottlieb@gmail.com>
2022-04-25 20:52:26 -0400

    attempt to change to 10

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Testing for NETS150 project!");
-        System.out.println("Test #3");
+        System.out.println("Test #10");
         System.out.println("CHANGED BY ETHAN!");
 
     }
