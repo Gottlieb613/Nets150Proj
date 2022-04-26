4814109690abc2f78755b039d5b8c14677e133e1
“Ethan71821 git config --global user.email “etheisenberg@gmail.com <63895333+Ethan71821@users.noreply.github.com>
2022-04-25 20:47:48 -0400

    Changed by Ethan

--- Test.java
+++ Test.java
@@ -2,5 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Testing for NETS150 project!");
         System.out.println("Test #2");
+        System.out.println("CHANGED BY ETHAN!");
+
     }
 }
