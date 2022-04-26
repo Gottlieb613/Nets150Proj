fde30ec5e2d1e22a4b5299df1877ae6243ac3d9d
Gottlieb613 <charliegottlieb@gmail.com>
2022-04-25 21:17:22 -0400

    added testInt (14)

--- main.java
+++ main.java
@@ -3,8 +3,17 @@ public class main {
         System.out.println("Hello hello");
         Test n1 = new Test("This code needs to be printed");
         n1.testPrint();
-        
+
         Test n2 = new Test("Print", 14);
         n2.testPrint();
+
+
+        int testInt = 14;
+
+
+        for (int i = 0; i < testInt; i++) {
+            System.out.print(i + " ");
+        }
+
     }
 }
