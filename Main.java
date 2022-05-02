b92b5779b1030ed77b673214b8d74ec92976df91
“Ethan71821 git config --global user.email “etheisenberg@gmail.com <63895333+Ethan71821@users.noreply.github.com>
2022-05-02 00:33:19 -0400

    edited the GUI further

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
-import java.util.Scanner;
+import jdk.nashorn.internal.scripts.JO;
 
+import javax.swing.*;
+import java.util.Scanner;
+import java.*;
 public class Main {
     private static Scanner scan;
 
@@ -10,83 +13,166 @@ public class Main {
     }
 
     static void open() {
-        System.out.println(
-                "Welcome to the UPenn CIS Course Catalog Info Center!\n" +
-                "(actual name TBD)\n" +
-                "Created by Ethan Eiseinberg, Charlie Gottlieb, and Hussain Zaidi\n" +
-                "Choose a question to ask below by typing in the respective number");
+        JFrame frame = new JFrame();
+        String userName = (String) JOptionPane.showInputDialog(
+                frame,
+                "Enter your name",
+                "Introduce yourself!",
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                null,
+                "Node A");
+        JOptionPane.showMessageDialog(frame, userName + ", Welcome to the UPenn CIS Course Catalog " +
+                "Info Center!\n" + "\n" +
+                "Created by Ethan Eisenberg, Charlie Gottlieb, and Hussain Zaidi\n" +
+
+                " To begin, please choose a question");
+
+
         askQuestions();
     }
 
+
     static void askQuestions() {
-        System.out.print(
+        String[] optionSet = new String[]{"5", "4", "3", "2", "1"};
+        JFrame frame = new JFrame();
+        int choice = JOptionPane.showOptionDialog(
+                frame,
                 "1. What does a particular subject code mean (i.e. CIS, NURS, DIGC)?\n" +
-                "2. Provide some information about a given course (i.e. CIS 121)\n" +
-                "3. Provide some information about CIS Tech Elective courses\n" +
-                "4. Provide some information about CIS Humanities courses\n" +
-                "5. What course would I be interested in within a particular subject?\n" +
-                "Type a question number here: ");
-        int choice = scan.nextInt();
-        scan.nextLine(); //this is to bring the scanner carriage to end of line for next scan
-        if (choice < 1 || choice > 5) {
-            System.out.println("That is not a valid option, please choose again");
+                        "2. Provide some information about a given course (i.e. CIS 121)\n" +
+                        "3. Provide some information about CIS Tech Elective courses\n" +
+                        "4. Provide some information about CIS Humanities courses\n" +
+                        "5. What course would I be interested in within a particular subject?\n",
+
+                "What are you interested in learning about?",
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                optionSet,
+                optionSet[4]
+        );
+
+
+//        int choice = scan.nextInt();
+//        scan.nextLine(); //this is to bring the scanner carriage to end of line for next scan
+
+
+        if (choice < 0 || choice > 4) {
+            JOptionPane.showMessageDialog(frame, "That is not a valid option, please choose again");
+
         } else {
             switch (choice) {
+                case 0: {
+                    question5();
+                    break;
+                }
                 case 1: {
-                    question1();
-                    break;
-                }
-                case 2: {
-                    question2();
-                    break;
-                }
-                case 3: {
-                    question3();
-                    break;
-                }
-                case 4: {
                     question4();
                     break;
                 }
-                case 5: {
-                    question5();
+                case 2: {
+                    question3();
+                    break;
+                }
+                case 3: {
+                    question2();
+                    break;
+                }
+                case 4: {
+                    question1();
                     break;
                 }
             }
         }
-        System.out.println("\n\nAsk another question? (y/n)");
-        String askAgain = scan.nextLine();
-        if (askAgain.equals("y")) {
+//        System.out.println("\n\nAsk another question? (y/n)");
+//        String askAgain = scan.nextLine();
+        String[] yesNoSet = new String[]{"Yes!", "No, thank you"};
+
+        int again = JOptionPane.showOptionDialog(
+                frame,
+                "Ask another question?",
+
+                "Ask again?",
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                yesNoSet,
+                yesNoSet[1]
+        );
+
+        if (again == 0) {
             askQuestions();
         } else {
-            System.out.println("Thank you for using our system. Have a nice day!");
+            JOptionPane.showMessageDialog(frame, "Thank you for using our system. Have a nice day!");
         }
     }
 
     static void question1() {
-        System.out.print("Type the subject code: ");
-        String code = scan.nextLine();
+        JFrame frame = new JFrame();
+        String code = (String) JOptionPane.showInputDialog(
+                frame,
+                "Type the subject code: ",
+                "Provide Subject Information",
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                null,
+                "NETS");
+
+//        System.out.print("Type the subject code: ");
+//        String code = scan.nextLine();
         String title = CourseInfo.getSubjectTitle(code);
         if (title != null) {
-            System.out.println(code.toUpperCase() + " refers to " + title);
+            JOptionPane.showMessageDialog(frame, code.toUpperCase() + " refers to " + title);
+//            System.out.println(code.toUpperCase() + " refers to " + title);
         }
     }
 
     static void question2() {
-        System.out.print("Type the course ID (i.e. CIS 110): ");
-        String id = scan.nextLine();
+//        System.out.print("Type the course ID (i.e. CIS 110): ");
+//        String id = scan.nextLine();
+
+        JFrame frame = new JFrame();
+        String id = (String) JOptionPane.showInputDialog(
+                frame,
+                "Type the course ID (i.e. NETS 150): ",
+                "Provide Subject Information",
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                null,
+                "NETS 150");
+
+
         Course course = CourseInfo.getCourseObj(id);
         if (course != null) {
-            System.out.print("\n" + id.toUpperCase() + " is " + course.getTitle() +
-            "\nIts description is:\n\t" + course.getDescriptionNewlines() +
-            "\nAnd its prerequisites are:\n\t");
+
+
             if (course.getPrereqs().size() == 0) {
-                System.out.println("No prerequisites");
-            } else {
-                System.out.println(course.getPrereqs());
+                JOptionPane.showMessageDialog(frame, "\n" + id.toUpperCase() + " is: " + course.getTitle() +
+                        "\nIts description is:\n\t" + course.getDescriptionNewlines() +
+                        "\n\n Further, there are no prerequisites. Get " +
+                        "ready to learn!");
             }
+
+
+////            System.out.print("\n" + id.toUpperCase() + " is " + course.getTitle() +
+////            "\nIts description is:\n\t" + course.getDescriptionNewlines() +
+////            "\nAnd its prerequisites are:\n\t");
+//            if (course.getPrereqs().size() == 0) {
+////                System.out.println("No prerequisites");
+//                JOptionPane.showMessageDialog(frame, "Further, there are no prerequisites. Get " +
+//                        "ready to learn!");
+         else {
+            JOptionPane.showMessageDialog(frame, "\n" + id.toUpperCase() + " is: " + course.getTitle() +
+                    "\n\nIts description is:\n\t" + course.getDescriptionNewlines() +
+                    "\n\nAnd its prerequisites are:\n\t " + course.getPrereqs());
+//                JOptionPane.showMessageDialog(frame, "Here are the  prerequisites to note: \n" +
+//                        course.getPrereqs() );
+//                System.out.println(course.getPrereqs());
         }
     }
+
+}
+
 
     static void question3() {
 
@@ -97,6 +183,9 @@ public class Main {
     }
 
     static void question5() {
+
+        VectorSpaceModelTester test = new VectorSpaceModelTester();
+        test.recommendationQuestion();
 
     }
 }
--- VectorSpaceModelTester.java
+++ VectorSpaceModelTester.java
@@ -16,11 +16,103 @@ import java.*;
 public class VectorSpaceModelTester {
 
 
+    public void retrieveTopListings (String departmentListing, String result, boolean providedCourse){
+        JFrame frame = new JFrame();
+        CourseInfo courseInfo = new CourseInfo();
+        ArrayList<Course> coursesInDepartment = courseInfo.getAllCoursesInSubject(departmentListing.toLowerCase());
+        ArrayList<Document> documentsArrayList = new ArrayList<>();
+        ArrayList<String> documentNames = new ArrayList<>();
+        //Write the course query (based on the code to a new file)
+        try {
+            //relative path to write to a file that is not specified
+            BufferedWriter writer = new BufferedWriter(
+                    new FileWriter("./Classes/classDescription.txt"));
+            writer.write(result);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Document queryCourseInput =  new Document("./Classes/classDescription.txt");
+        documentsArrayList.add(queryCourseInput);
+        documentNames.add(result);
+
+
+
+        //add all the descriptions of the courses in the department
+        for (Course c: coursesInDepartment){
+            /*only add courses that have a different description (we don't want to recommend courses
+            the user has already provided */
+            if (!c.getDescription().equals(result)) {
+                //Write the course query (based on the code to a new file)
+                try {
+                    //relative path to write to a file that is not specified
+                    BufferedWriter writer = new BufferedWriter(
+                            new FileWriter("./Classes/classDescription.txt"));
+                    writer.write(c.getDescription());
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Document add = new Document("./Classes/classDescription.txt");
+                documentsArrayList.add(add);
+                documentNames.add(c.getTitle());
+            }
+        }
+
+
+
+        //
+        TreeMap< Double, String> mapFromCourseToSimilarity = new TreeMap<>();
+
+        Corpus corpus = new Corpus(documentsArrayList);
+        VectorSpaceModel vectorSpace = new VectorSpaceModel(corpus);
+        for(int i = 1; i < documentsArrayList.size(); i++) {
+            Document doc = documentsArrayList.get(i);
+            String name = documentNames.get(i);
+            System.out.println("\nComparing to " + name);
+            System.out.println(vectorSpace.cosineSimilarity(queryCourseInput, doc));
+
+            //put the values to the tree map
+            mapFromCourseToSimilarity.put( vectorSpace.cosineSimilarity(queryCourseInput, doc), name);
+        }
+
+        Set<Double> values = mapFromCourseToSimilarity.keySet();
+        ArrayList<Double> valuesToSort = new ArrayList<>();
+        for (Double val: values){
+            valuesToSort.add(val);
+        }
+        Collections.sort(valuesToSort);
+        Collections.reverse(valuesToSort);
+
+        String displayString = "";
+        for (int i = 0; i < 5; i++){
+            if (i < valuesToSort.size()) {
+                displayString += "\n" + (i + 1) + ". " + mapFromCourseToSimilarity.get(valuesToSort.get(i)) +
+                        ";" + " " + valuesToSort.get(i);
+            }
+        }
 
 
 
 
-    public static void main(String[] args) {
+
+        if (!providedCourse) {
+            JOptionPane.showMessageDialog(frame, "Beep Bop.. " +
+                    "The Top  Courses In The  " + departmentListing.toUpperCase() + " department that" +
+                    " are the most similar to the query,   " + result +
+                    ", \n" + " are: " + "\n" + displayString);
+        }
+        else{
+            JOptionPane.showMessageDialog(frame, "Beep Bop.. " +
+                    "The Top  Courses In The  " + departmentListing.toUpperCase() + " department that" +
+                    " are the most similar to the provided course  "  +
+                    "\n" + " are: " + "\n" + displayString);
+        }
+
+    }
+
+
+    public void recommendationQuestion() {
 
         CourseInfo courseInfo = new CourseInfo();
         JFrame frame = new JFrame();
@@ -31,8 +123,8 @@ public class VectorSpaceModelTester {
 
         //would you want to filter by course code or course description
         JOptionPane.showMessageDialog(frame, "Welcome to Course Recommender! ");
-        String[] optionSet = new String[] { "Filter by Course Code", "Filter by Course Description" };
-        int  n = JOptionPane.showOptionDialog(
+        String[] optionSet = new String[]{"Filter by Course Code", "Filter by Course Description"};
+        int n = JOptionPane.showOptionDialog(
                 frame,
                 "How do you want to receive your course recommendations?",
                 "Choose your method of recommendation!",
@@ -43,12 +135,12 @@ public class VectorSpaceModelTester {
                 optionSet[1]
         );
 
-        if (n == 0){
+        if (n == 0) {
 
             //Retrieve the department that the user inputs
             JOptionPane.showMessageDialog(frame, "First, please enter the department code that you want to " +
                     "receive recommendations for!");
-            String departmentListing = (String)JOptionPane.showInputDialog(
+            String departmentListing = (String) JOptionPane.showInputDialog(
                     frame,
                     "Enter the  Department Category",
                     "Recommendation",
@@ -58,112 +150,34 @@ public class VectorSpaceModelTester {
                     "cis");
             JOptionPane.showMessageDialog(frame, "You selected the " + departmentListing.toUpperCase()
                     + " department");
-                JOptionPane.showMessageDialog(frame, "Please enter the " +
-                        " course code that you want to receive similar courses for! ");
-                String result = (String) JOptionPane.showInputDialog(
-                        frame,
-                        "Enter the Course Code",
-                        "Recommendation",
-                        JOptionPane.PLAIN_MESSAGE,
-                        null,
-                        null,
-                        "CIS 099"
-                );
+            JOptionPane.showMessageDialog(frame, "Please enter the " +
+                    " course code that you want to receive similar courses for! ");
+            String result = (String) JOptionPane.showInputDialog(
+                    frame,
+                    "Enter the Course Code",
+                    "Recommendation",
+                    JOptionPane.PLAIN_MESSAGE,
+                    null,
+                    null,
+                    "CIS 099"
+            );
 
-                //assume for now that they entered a code that is cis
-            Course  selectedCourse = CourseInfo.getCourseObj(result);
+            //assume for now that they entered a code that is cis
+            Course selectedCourse = CourseInfo.getCourseObj(result);
             JOptionPane.showMessageDialog(frame, "We will now receive recommendations for: " + " \n" +
                     selectedCourse.getTitle());
-            ArrayList<Course> coursesInDepartment = courseInfo.getAllCoursesInSubject(departmentListing.toLowerCase());
-            ArrayList<Document> documentsArrayList = new ArrayList<>();
-            ArrayList<String> documentNames = new ArrayList<>();
-            //Write the course query (based on the code to a new file)
-            try {
-                //relative path to write to a file that is not specified
-                BufferedWriter writer = new BufferedWriter(
-                        new FileWriter("./Classes/classDescription.txt"));
-                writer.write(selectedCourse.getDescription());
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            Document queryCourseInput =  new Document("./Classes/classDescription.txt");
-            documentsArrayList.add(queryCourseInput);
-            documentNames.add(selectedCourse.getTitle());
+            retrieveTopListings(departmentListing, selectedCourse.getDescription(), true);
 
 
 
 
-           //add all the descriptions of the courses in the department
-            for (Course c: coursesInDepartment){
-                //make sure that we are not adding the same course description as the one that the user
-                //is trying to receive recommendations for
-                if (!c.getTitle().equals(selectedCourse.getTitle())) {
-                    //Write the course query (based on the code to a new file)
-                    try {
-                        //relative path to write to a file that is not specified
-                        BufferedWriter writer = new BufferedWriter(
-                                new FileWriter("./Classes/classDescription.txt"));
-                        writer.write(c.getDescription());
-                        writer.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    Document add =  new Document("./Classes/classDescription.txt");
-                    documentsArrayList.add(add);
-                    documentNames.add(c.getTitle());
-                }
-            }
-
-
-            //
-            TreeMap< Double, String> mapFromCourseToSimilarity = new TreeMap<>();
-
-            Corpus corpus = new Corpus(documentsArrayList);
-            VectorSpaceModel vectorSpace = new VectorSpaceModel(corpus);
-            for(int i = 1; i < documentsArrayList.size(); i++) {
-                Document doc = documentsArrayList.get(i);
-                String name = documentNames.get(i);
-                System.out.println("\nComparing to " + name);
-                System.out.println(vectorSpace.cosineSimilarity(queryCourseInput, doc));
-
-                //put the values to the tree map
-                mapFromCourseToSimilarity.put( vectorSpace.cosineSimilarity(queryCourseInput, doc), name);
-            }
-
-          Set<Double> values = mapFromCourseToSimilarity.keySet();
-            ArrayList<Double> valuesToSort = new ArrayList<>();
-            for (Double val: values){
-                valuesToSort.add(val);
-            }
-            Collections.sort(valuesToSort);
-            Collections.reverse(valuesToSort);
-
-            String displayString = "";
-            for (int i = 0; i < 5; i++){
-                if (valuesToSort.size() > i) {
-                    displayString += "\n" + (i + 1) + ". " + mapFromCourseToSimilarity.get(valuesToSort.get(i)) +
-                            ";" + " " + valuesToSort.get(i);
-                }
-            }
-
-
-
-
-            JOptionPane.showMessageDialog(frame, "Beep Bop.. The Top Courses That Are The Most Similar To " +
-                    " \n" + selectedCourse.getTitle() + " are: " + "\n" + displayString);
-
-
-
-        }
-
-        else if (n == 1){
+        } else if (n == 1) {
 
 
             //Retrieve the department that the user inputs
             JOptionPane.showMessageDialog(frame, "First, please enter the department code that you want to " +
                     "receive recommendations for!");
-            String departmentListing = (String)JOptionPane.showInputDialog(
+            String departmentListing = (String) JOptionPane.showInputDialog(
                     frame,
                     "Enter the  Department Category",
                     "Recommendation",
@@ -176,7 +190,7 @@ public class VectorSpaceModelTester {
 
 
             JOptionPane.showMessageDialog(frame, "Now, please enter text that matches your interests");
-            String result = (String)JOptionPane.showInputDialog(
+            String result = (String) JOptionPane.showInputDialog(
                     frame,
                     "Enter the Course Description ",
                     "Recommendation",
@@ -186,86 +200,11 @@ public class VectorSpaceModelTester {
                     "Artificial Intelligence and  Machine Learning"
             );
 
+            retrieveTopListings(departmentListing, result, false);
 
-            ArrayList<Course> coursesInDepartment = courseInfo.getAllCoursesInSubject(departmentListing.toLowerCase());
-            ArrayList<Document> documentsArrayList = new ArrayList<>();
-            ArrayList<String> documentNames = new ArrayList<>();
-            //Write the course query (based on the code to a new file)
-            try {
-                //relative path to write to a file that is not specified
-                BufferedWriter writer = new BufferedWriter(
-                        new FileWriter("./Classes/classDescription.txt"));
-                writer.write(result);
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            Document queryCourseInput =  new Document("./Classes/classDescription.txt");
-            documentsArrayList.add(queryCourseInput);
-            documentNames.add(result);
-
-
-
-
-            //add all the descriptions of the courses in the department
-            for (Course c: coursesInDepartment){
-                    //Write the course query (based on the code to a new file)
-                    try {
-                        //relative path to write to a file that is not specified
-                        BufferedWriter writer = new BufferedWriter(
-                                new FileWriter("./Classes/classDescription.txt"));
-                        writer.write(c.getDescription());
-                        writer.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    Document add =  new Document("./Classes/classDescription.txt");
-                    documentsArrayList.add(add);
-                    documentNames.add(c.getTitle());
-                }
-
-
-
-            //
-            TreeMap< Double, String> mapFromCourseToSimilarity = new TreeMap<>();
-
-            Corpus corpus = new Corpus(documentsArrayList);
-            VectorSpaceModel vectorSpace = new VectorSpaceModel(corpus);
-            for(int i = 1; i < documentsArrayList.size(); i++) {
-                Document doc = documentsArrayList.get(i);
-                String name = documentNames.get(i);
-                System.out.println("\nComparing to " + name);
-                System.out.println(vectorSpace.cosineSimilarity(queryCourseInput, doc));
-
-                //put the values to the tree map
-                mapFromCourseToSimilarity.put( vectorSpace.cosineSimilarity(queryCourseInput, doc), name);
-            }
-
-            Set<Double> values = mapFromCourseToSimilarity.keySet();
-            ArrayList<Double> valuesToSort = new ArrayList<>();
-            for (Double val: values){
-                valuesToSort.add(val);
-            }
-            Collections.sort(valuesToSort);
-            Collections.reverse(valuesToSort);
-
-            String displayString = "";
-            for (int i = 0; i < 5; i++){
-                if (i < valuesToSort.size()) {
-                    displayString += "\n" + (i + 1) + ". " + mapFromCourseToSimilarity.get(valuesToSort.get(i)) +
-                            ";" + " " + valuesToSort.get(i);
-                }
-            }
-
-
-
-
-            JOptionPane.showMessageDialog(frame, "Beep Bop.. " +
-                    "The Top  Courses In The  " + departmentListing.toUpperCase() + " department that" +
-                    " are the most similar to the query,  " +    result   +
-                    ", \n" + " are: " + "\n" + displayString);
 
         }
-    }
 
+    }
 }
+
